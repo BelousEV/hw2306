2f7b5a67ad2b925b1e8d279f1d242f9121165dd4
ElenaR <llinusik@mail.ru>
2022-06-23 16:26:33 +0300

    Initial commit

---
+++ src/main/java/com/example/hw2306/Hw2306Application.java
@@ -0,0 +1,13 @@
+package com.example.hw2306;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw2306Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw2306Application.class, args);
+    }
+
+}
